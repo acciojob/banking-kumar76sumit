a4cda5138a3181f72a20a4c09693de40a57d0c70
Sumit <kumar76sumit@gmail.com>
2023-11-24 00:24:31 +0530

    fasga

--- src/main/java/com/driver/CurrentAccount.java
+++ src/main/java/com/driver/CurrentAccount.java
@@ -23,7 +23,7 @@ public class CurrentAccount extends BankAccount{
         // minimum balance is 5000 by default. If balance is less than 5000, throw "Insufficient Balance" exception
         super(name,balance,5000);
         this.tradeLicenseId=tradeLicenseId;
-        if(balance<5000)
+        if(getBalance()<5000)
         {
             throw new Exception("Insufficient Balance");
         }
