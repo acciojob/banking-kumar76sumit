706e62164d84ae9b28d9160be43249087fbec24e
Sumit <kumar76sumit@gmail.com>
2023-11-24 00:34:52 +0530

    asgasga

--- src/main/java/com/driver/SavingsAccount.java
+++ src/main/java/com/driver/SavingsAccount.java
@@ -26,11 +26,7 @@ public class SavingsAccount extends BankAccount{
         {
             throw new Exception("Maximum Withdraw Limit Exceed");
         }
-        if(amount>getBalance())
-        {
-            throw new Exception("Insufficient Balance");
-        }
-        setBalance(getBalance()-amount);
+        super.withdraw(amount);
     }
 
     public double getSimpleInterest(int years){
